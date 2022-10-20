cce9d8945fccf5278e8eb9cb513b0a0f87bfb1cb
Helen <helenmperham@gmail.com>
2022-10-20 17:03:54 -0400

    commit tree cd060cfab891942bd77fc9fe99107e60d0a8089d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const cardInside = [];
+    for (let i = 0; i < names.length; i++) {
+     const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+     cardInside.push(message);
+
+    };
+    return cardInside;
+}
+console.log(cardInside);
+
+
+function countDown(posiInteger) {
+    let i = posiInteger
+    while (i >= 0){
+        console.log(i)
+        i--; 
+    }
+}
+countDown(10);
